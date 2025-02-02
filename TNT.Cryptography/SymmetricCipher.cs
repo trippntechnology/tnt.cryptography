1147ed8cdf9778dec39fb60f4ba47f691a6c89d4
Steve Tripp <steve@trippntechnology.com>
2025-02-01 17:22:20 -0700

    Add missing changes

--- TNT.Cryptography/SymmetricCipher.cs
+++ TNT.Cryptography/SymmetricCipher.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 
 namespace TNT.Cryptography;
 
@@ -39,10 +41,24 @@ public class SymmetricCipher : BaseCipher
   }
 
   /// <summary>
+  /// Decrypts <paramref name="encodedString"/> and deserializes it into an object of <typeparamref name="T"/> 
+  /// </summary>
+  /// <typeparam name="T">Type of object represented by the encoded string</typeparam>
+  /// <param name="encodedString">Base64 encoded string representing encrypted bytes</param>
+  /// <returns>Object of <typeparamref name="T"/> on success, null otherwise</returns>
+  public override T? Decrypt<T>(string encodedString) where T : default
+  {
+    var encryptedBytes = Convert.FromBase64String(encodedString);
+    var decryptedBytes = Decrypt(encryptedBytes);
+    var decryptedString = Encoding.UTF8.GetString(decryptedBytes);
+    return JsonSerializer.Deserialize<T>(decryptedString);
+  }
+
+  /// <summary>
   /// Encrypts <paramref name="plainTextBytes"/>
   /// </summary>
   /// <returns><see cref="byte"/> array of encrypted bytes</returns>
-  public override byte[] Encypt(byte[] plainTextBytes)
+  public override byte[] Encrypt(byte[] plainTextBytes)
   {
     using (Aes aes = Aes.Create())
     {
@@ -62,5 +78,17 @@ public class SymmetricCipher : BaseCipher
         }
       }
     }
+  }
+
+  /// <summary>
+  /// Serialize and encrypts <paramref name="obj"/>
+  /// </summary>
+  /// <returns>Base64 encoded string that represents <paramref name="obj"/></returns>
+  public override string Encrypt(object obj)
+  {
+    var serializedObj = JsonSerializer.Serialize(obj);
+    var serializedBytes = Encoding.UTF8.GetBytes(serializedObj);
+    var encryptedBytes = Encrypt(serializedBytes);
+    return Convert.ToBase64String(encryptedBytes);
   }
 }
